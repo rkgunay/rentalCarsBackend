7a640678c7a9c9ed08f91d313c232871707a0e0d
rkgunay <ridvankgunay@gmail.com>
2021-10-04 23:14:40 +0300

    Business katmanına Result yapılandırması eklendi.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Araba eklendi";
+        public static string CarEntryInvalid = "Hatalı bilgi girişi yaptınız";
+    }
+}

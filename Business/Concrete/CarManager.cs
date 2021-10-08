c360bc91245726557047e8c91b53bb7b41bf319c
rkgunay <ridvankgunay@gmail.com>
2021-10-09 01:43:12 +0300

    Ders12 Odev1,2,3 kodları eklendi. Autofac, FluentValidation, ValidationAspect. eklendi.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -19,14 +21,14 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if(car.Name.Length>2 && car.DailyPrice > 0)
-            {
+            //Business Codes Below 
+
             _carDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
-            }
-            else return new ErrorResult(Messages.CarEntryInvalid);
+            return new SuccessResult(Messages.CarAdded);  
+           
         }
 
 
---
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator : AbstractValidator<Rental>
+    {
+
+    }
+}
